d2fe836435a37d2471bc14612ce35e662a2475e0
Marcus10110 <markgarrison@saleae.com>
2013-12-15 12:25:52 -0800

    Joe's changes to Cell BMS code: He Added Condition Critical (red led) expanded idle timer to 30 seconds added system to turn off cell bleed current after idle timer expired. -needs temperature critical levels. -needs to use SetDac in timer ISR

--- CellBms/main.c
+++ CellBms/main.c
@@ -26,9 +26,20 @@ U16 WatchDogCounterS = 0;
 U16 IdlePeriodCounterS = 0;
 bool BmsAbortWatchdog = FALSE;
 U8 tx_data[7] = {0x03, 0x01, 0x0F, 0xAA };
+bool Discharging = FALSE;
 
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+U8 ConditionCritical = 0;
+#define CONDITION_CRITICAL_VOLTAGE 1
+#define CONDITION_CRITICAL_TEMP 2
+#define CONDITION_CRITICAL_UNEXPECTED 4
+
+
+int main(void)
+{
+	//WDTCTL = WDTPW | WDTCNTCL;	// clear WDT.  SMCLK as clock, divide by 32768.  244Hz.
+	WDTCTL = WDTPW | WDTHOLD;
+
+
     DCOCTL = CALDCO_8MHZ,
     BCSCTL1 = CALBC1_8MHZ;
 
@@ -49,7 +60,7 @@ int main(void) {
     TACTL = TASSEL_2 + MC_1 + ID_0;  //TASSEL_2=SMCLK, MC_1=Up mode: the timer counts up to TACCR0, ID_0=div by 1
 
     InitI2C( 0x60 );
-    InitAdcCell(BIT0 | BIT1 ); //VBATT and NTC
+    InitAdcCell( BIT0 | BIT1 ); //VBATT and NTC
 
     SetDac(0); //full scale range is 0 to 2.5V, 10 bit (1024)
 
@@ -59,7 +70,6 @@ int main(void) {
 
 
     __bis_SR_register( GIE );
-
 
 
     //U16 batt_voltage = 0;
@@ -73,20 +83,23 @@ int main(void) {
     	//U16 i;
 
     	//set these twice, just in case they are being modified by the timer.
-    	WatchDogCounterS = 0;
-    	BmsAbortWatchdog = FALSE;
-    	WatchDogCounterS = 0;
-    	BmsAbortWatchdog = FALSE;
+    	while( WatchDogCounterS != 0 )
+    		WatchDogCounterS = 0;
+
+    	while( BmsAbortWatchdog != 0 )
+    		BmsAbortWatchdog = FALSE;
 
     	//lets wait for a command from below.
-    	while( rx_length == 0)
+    	while( rx_length == 0 )
     	{
 			StartReadData(FALSE); //read from down.
-			while(!CheckOperationComplete());
+			while( !CheckOperationComplete() );
 			rx_length = RxPacketSize;
     	}
+
     	IdlePeriodCounterS = 0;
     	ProcessCommand();
+
     	/*tx_data[0] = CELL_CMD_PING;
     	tx_data[1] = 0;
     	WriteDataBmsSerial(tx_data,2,TRUE);
@@ -130,7 +143,7 @@ void ProcessCommand()
 			tx_data[0] = CELL_CMD_PING;
 			tx_data[1] = 0x01; //PARAM_ON
 			WriteDataBmsSerial(tx_data,2,FALSE);
-			while(!CheckOperationComplete());
+			while( !CheckOperationComplete() );
 			return;
 		}
 
@@ -139,9 +152,8 @@ void ProcessCommand()
 		tx_data[0] = CELL_CMD_PING;
 		tx_data[1] = cell_id_in;
 		PassCommandUp(2);
-
-
 		break;
+
 	case CELL_CMD_GET_TEMP:
 
 		if( RxPacketSize != 3) //packet length, command, id.
@@ -152,6 +164,14 @@ void ProcessCommand()
 		if( cell_id_in == 0 )
 		{
 			temperature = GetTemperature();
+
+			/*  todo: compute these limits
+			if( ( temperature > 1000 ) || ( temperature < 400 ) )  //30deg and 130deg
+				ConditionCritical |= CONDITION_CRITICAL_TEMP;
+			else
+				ConditionCritical &= ~CONDITION_CRITICAL_TEMP;
+			*/
+
 			tx_data[0] = CELL_CMD_GET_TEMP;
 			tx_data[1] = temperature >> 8;
 			tx_data[2] = temperature & 0x00FF;
@@ -179,6 +199,12 @@ void ProcessCommand()
 		if( cell_id_in == 0 )
 		{
 			batt_voltage = GetBattVoltage();
+
+			if( batt_voltage < 715 )  //about 2.8V
+				ConditionCritical |= CONDITION_CRITICAL_VOLTAGE;
+			else
+				ConditionCritical &= ~CONDITION_CRITICAL_VOLTAGE;
+
 			tx_data[0] = CELL_CMD_GET_VOLTAGE;
 			tx_data[1] = batt_voltage >> 8;
 			tx_data[2] = batt_voltage & 0x00FF;
@@ -207,13 +233,19 @@ void ProcessCommand()
 			dac_value = Buffer[3] << 8; //high
 			dac_value |= Buffer[4]; //low
 
+			if( dac_value != 0 )
+				Discharging = TRUE;
+			else
+				Discharging = FALSE;
+
 			SetDac( dac_value );
 			//batt_voltage = GetBattVoltage();
 			tx_data[0] = CELL_CMD_SET_CURRENT;
 			tx_data[1] = 0x01; //PARAM ON
 
 			WriteDataBmsSerial(tx_data,2,FALSE);
-			while(!CheckOperationComplete());
+			while( !CheckOperationComplete() );
+
 			return;
 		}
 
@@ -285,6 +317,7 @@ void InitAdcCell( U16 active_inputs )
 	ADC10AE0 |= (active_inputs & 0x00FF);
 
 	ADC10CTL0 = ADC10SHT_3 + ADC10ON + SREF_3 + REFBURST; 	//ADC10SHT_3=64 × ADC10CLKs, SREF_3=VR+ = buffered VEREF+ and VR- = VSS, reference in burst mode.
+
 	ADC10CTL1 = INCH_1 + ADC10SSEL_3 + ADC10DIV_7; 			//INCH_1 is just a place holder here.
 }
 
@@ -292,8 +325,9 @@ void InitAdcCell( U16 active_inputs )
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer_A (void)
 {
-	TimerEventBmsSerial();
+	//WDTCTL = WDTPW | WDTCNTCL;  //clear WDT.  Pretty crappy place to clear the WDT since everything else could be FUBAR and we could this timer could still get called.  Ideally we would catch a hang in the main loop.
 
+	TimerEventBmsSerial();
 
 	Us += 1000;
 
@@ -304,14 +338,34 @@ __interrupt void Timer_A (void)
 	}
 
 	if( (Ms > 0) && (Ms < 50))
+	{
 		GREEN_ON;
+		if( Discharging == TRUE )
+			RED_ON;
+	}
 	else
+	{
 		GREEN_OFF;
+		if( Discharging == FALSE )
+			RED_OFF;
+	}
 
-	if( IdlePeriodCounterS > 10 )
+	if( IdlePeriodCounterS > 30 )
+	{
+		//stop charging.
+		//U8 buffer[2];
+		//buffer[0] = 0;
+		//buffer[1] = 0;
+		//WriteMultipleI2c( 0x60, buffer, 2 );
+		SetDac(0);
+		Discharging = FALSE;
+
 		YELLOW_ON;
+	}
 	else
+	{
 		YELLOW_OFF;
+	}
 
 	if( Ms >= 1000 )
 	{
@@ -329,15 +383,14 @@ __interrupt void Timer_A (void)
 
 	}
 
-
-
-
-
+	if( ConditionCritical != 0 )
+		RED_ON;
 }
 
-#pragma vector=PORT1_VECTOR, PORT2_VECTOR, ADC10_VECTOR, USCIAB0TX_VECTOR, WDT_VECTOR, NMI_VECTOR, COMPARATORA_VECTOR, TIMER0_A1_VECTOR, TIMER1_A0_VECTOR, TIMER1_A1_VECTOR, USCIAB0RX_VECTOR//, RESET_VECTOR
+#pragma vector=PORT1_VECTOR, PORT2_VECTOR, ADC10_VECTOR, USCIAB0TX_VECTOR, NMI_VECTOR, WDT_VECTOR, COMPARATORA_VECTOR, TIMER0_A1_VECTOR, TIMER1_A0_VECTOR, TIMER1_A1_VECTOR, USCIAB0RX_VECTOR//, RESET_VECTOR
 __interrupt void TRAP_ISR(void)
 {
+	ConditionCritical |= CONDITION_CRITICAL_UNEXPECTED;
 	RED_ON;
 }
 
